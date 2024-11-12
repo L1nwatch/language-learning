630852d6a7fbcb1e4bf72a058da4ff20030d6414
L1nwatch <490772448@qq.com>
2024-11-11 21:36:58 -0500

    support "word" type error

--- Review.py
+++ Review.py
@@ -5,7 +5,7 @@
 """
 import os
 from pydub import AudioSegment
-
+from gtts import gTTS
 
 __author__ = '__L1n__w@tch'
 
@@ -18,12 +18,18 @@ class Review:
         return (minutes * 60 + seconds) * 1000
 
     def prepare_audio_segment(self, info):
-        audio_file = os.path.join(self.listening_root, info["file"])
-        start_time, end_time = info["start_time"], info["end_time"]
-        audio = AudioSegment.from_file(audio_file)
-        segment = audio[self._time_to_milliseconds(start_time):self._time_to_milliseconds(end_time)]
-        temp_file = "static/segment.mp3"
-        segment.export(temp_file, format="mp3")
-        return temp_file
-
+        if info["type"] == "listening":
+            audio_file = os.path.join(self.listening_root, info["file"])
+            start_time, end_time = info["start_time"], info["end_time"]
+            audio = AudioSegment.from_file(audio_file)
+            segment = audio[self._time_to_milliseconds(start_time):self._time_to_milliseconds(end_time)]
+            temp_file = "static/segment.mp3"
+            segment.export(temp_file, format="mp3")
+            return temp_file
+        elif info["type"] == "word":
+            word = info["answer"]
+            tts = gTTS(text=word, lang="en")
+            temp_file = "static/segment.mp3"
+            tts.save(temp_file)
+            return temp_file
 
